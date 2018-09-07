593883d2421ca34db72c2c9889b31b9545715c92
Atalya Yoseba <atalya.yoseba@ui.ac.id>
2018-09-07 18:27:46 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,22 @@
+public class Mahasiswa {
+	private String nama;
+	private String NPM;
+	
+	public Mahasiswa(String nama, String nPM) {
+		this.nama = nama;
+		NPM = nPM;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNPM() {
+		return NPM;
+	}
+	public void setNPM(String nPM) {
+		NPM = nPM;
+	}
+
+}
